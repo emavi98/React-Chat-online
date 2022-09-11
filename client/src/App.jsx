10872be0053e87d434bd7bd6007d48ce59f8289f
emavi98 <emanuelvm04@gmail.com>
2022-09-11 15:23:25 +0200

    react chat app build with Sockets.io and Node Js

---
+++ client/src/App.jsx
@@ -0,0 +1,66 @@
+/* eslint-disable react/jsx-key */
+import { useState, useEffect } from 'react';
+import './App.css';
+import io from 'socket.io-client';
+
+const socket = io('http://localhost:4000', { transports: ['websocket'] });
+
+function App() {
+	const [message, setMessage] = useState('');
+	const [messages, setMessages] = useState([]);
+
+	const handleSubmit = e => {
+		e.preventDefault();
+		socket.emit('message', message);
+		const newMessage = {
+			body: message,
+			from: 'Me',
+		};
+		setMessages([newMessage, ...messages]);
+		setMessage('');
+	};
+
+	useEffect(() => {
+		const receiveMessage = message => {
+			setMessages([message, ...messages]);
+		};
+
+		socket.on('message', receiveMessage);
+
+		return () => {
+			socket.off('message', receiveMessage);
+		};
+	}, [messages]);
+
+	return (
+		<div className='h-screen bg-zinc-800 text-white flex items-center justify-center'>
+			<form onSubmit={handleSubmit} className='bg-zinc-900 p-10'>
+				<h1 className='text-2xl font-bold my-2'>Chat React</h1>
+				<input
+					type='text'
+					onChange={e => setMessage(e.target.value)}
+					value={message}
+					className='border-2 border-zinc-5000 p-2 text-black w-full'
+				/>
+				{/* <button className='bg-blue-500'>Send</button> */}
+
+				<ul className='h-80 overflow-y-auto'>
+					{messages.map((message, index) => (
+						<li
+							key={index}
+							className={`my-2 p-2 table text-sm rounded-md ${
+								message.from === 'Me' ? 'bg-sky-700 ml-auto' : 'bg-black'
+							}`}
+						>
+							<p>
+								{message.from}: {message.body}
+							</p>
+						</li>
+					))}
+				</ul>
+			</form>
+		</div>
+	);
+}
+
+export default App;
---
+++ server/index.js
@@ -0,0 +1,37 @@
+import express from "express";
+import morgan from "morgan";
+import {Server as SocketServer} from "socket.io";
+import http from 'http';
+import cors from 'cors';
+import {PORT} from './config.js';
+import {dirname, join} from 'path';
+import {fileURLToPath} from 'url'
+
+const app = express();
+const __dirname = dirname(fileURLToPath(import.meta.url)); /* dirname("../client/dist") */
+console.log(__dirname)
+const server = http.createServer(app);
+const io = new SocketServer(server);
+
+app.use(cors())
+app.use(morgan("dev", {
+    cors: {
+        origin: 'http://localhost:5173'
+    }
+}));
+
+io.on('connection', (socket) => {
+    console.log(socket.id)
+    //console.log('a user connected')
+    socket.on('message', (message) => {
+        socket.broadcast.emit('message', {
+            body: message,
+            from: socket.id
+        }) 
+    })
+     
+})
+
+app.use(express.static(join(__dirname, '../client/dist')))
+server.listen(PORT)
+console.log('Server started on port', PORT);
